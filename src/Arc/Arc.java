91b91abd3d3c462f70484270d09419d84076bdf2
XIN Yuhua <yuhua.xin@imt-atlantique.net>
2021-10-05 16:55:12 +0800

    version1

--- src/Arc/Arc.java
+++ src/Arc/Arc.java
@@ -1,4 +1,8 @@
 package Arc;
+
+import PetriNetwork.Place;
+import PetriNetwork.Transition;
+
 /**
  * 
  */
@@ -7,9 +11,22 @@ package Arc;
  * @Description
  * @author wenjie fu email:wjfuxidian@163.com
  * @version
- * @date 2021年10月5日上午9:53:37
+ * @date 2021骞�10鏈�5鏃ヤ笂鍗�9:53:37
  * 
  */
 public class Arc {
+	
+	private Place place;
+	private Transition transition;
+	private int tokenInArc;
+	
+	public Arc(Place place, Transition transition) {
+		super();
+		this.place = place;
+		this.transition = transition;
+	}
+	
+	
+	
 
 }
--- src/Arc/ArcEnteredNormal.java
+++ src/Arc/ArcEnteredNormal.java
@@ -3,13 +3,30 @@
  */
 package Arc;
 
+import PetriNetwork.Place;
+import PetriNetwork.Transition;
+
 /**
  * @Description
  * @author wenjie fu email:wjfuxidian@163.com
  * @version
- * @date 2021年10月5日上午9:55:35
+ * @date 2021骞�10鏈�5鏃ヤ笂鍗�9:55:35
  * 
  */
-public class ArcEnteredNormal {
+public class ArcEnteredNormal extends ArcEntered {
+	
+	private int tokenInArc;
+
+	public ArcEnteredNormal(Place place, Transition transition) {
+		super(place, transition);
+	}
+
+
+	public ArcEnteredNormal(int tokenInArc, Place place, Transition transition) {
+		super(place, transition);
+		// TODO Auto-generated constructor stub
+		this.tokenInArc=tokenInArc;
+	}
+	
 
 }
